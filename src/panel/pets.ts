2be2318d1e0a42ddafc7434ea7c39eda55b5adbd
Blair Jordan <blair.j.jordan@gmail.com>
2023-04-17 16:56:33 +0800

    feat: character scaling

--- src/panel/pets.ts
+++ src/panel/pets.ts
@@ -482,6 +482,7 @@ export const getPetAnimations = ({
   }
 }
 
+// TODO: Set scale (passed from settings)
 export const generatePet = ({ name, type }: UserPetArgs): UserPet => ({
   leftPosition: 0,
   speed: 0,
@@ -493,6 +494,7 @@ export const generatePet = ({ name, type }: UserPetArgs): UserPet => ({
   isTransitionIn: true,
   name,
   type,
+  scale: 1, // TODO: require scale (passed from settings)
 })
 
 export const getLevel = ({
